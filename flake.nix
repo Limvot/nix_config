4b2685f7e1902e10f96b43e5be9061d38e1be317
Nathan Braswell <nathan@braswell.email>
2023-10-18 18:08:08 -0400

    inital framework

--- flake.nix
+++ flake.nix
@@ -341,6 +341,53 @@
                   networking.firewall.enable = false;
           }));
         in {
+        nixosConfigurations.nixos-framework = nixpkgs.lib.nixosSystem {
+            inherit system;
+            specialArgs = attrs;
+            modules = [
+                home-manager.nixosModules.home-manager
+                homeManagerSharedModule
+                ({ config, lib, pkgs, modulesPath, ... }@innerArgs: (lib.recursiveUpdate (commonConfigFunc innerArgs [ pkgs.light pkgs.gpodder pkgs.evince ]) {
+                  # HARDWARE
+                  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+                  
+                  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "usb_storage" "sd_mod" ];
+                  boot.initrd.kernelModules = [ ];
+                  boot.kernelModules = [ "kvm-amd" ];
+                  boot.extraModulePackages = [ ];
+                  
+                  fileSystems."/" =
+                    { device = "/dev/disk/by-uuid/427e2f6d-d42d-4d49-be35-713bf9526dc9";
+                      fsType = "ext4";
+                    };
+                  
+                  fileSystems."/boot" =
+                    { device = "/dev/disk/by-uuid/2A78-5373";
+                      fsType = "vfat";
+                    };
+                  
+                  swapDevices =
+                    [ { device = "/dev/disk/by-uuid/9b0357e8-f721-4a06-aae0-97b6efc19209"; }
+                    ];
+                  
+                  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+                  # (the default) this is the recommended approach. When using systemd-networkd it's
+                  # still possible to use this option, but it's recommended to use it in conjunction
+                  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+                  networking.useDHCP = lib.mkDefault true;
+                  # networking.interfaces.enp195s0f3u1c2.useDHCP = lib.mkDefault true;
+                  
+                  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+                  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+                  # END HARDWARE
+                  boot.loader.systemd-boot.enable = true;
+                  boot.loader.efi.canTouchEfiVariables = true;
+                  boot.kernelPackages = pkgs.linuxPackages_latest;
+                  networking.hostName = "nixos-framework"; # Define your hostname.
+                  system.stateVersion = "22.11"; # Did you read the comment?
+                }))
+            ];
+        };
         nixosConfigurations.nixos4800H = nixpkgs.lib.nixosSystem {
             inherit system;
             specialArgs = attrs;
